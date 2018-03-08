000452370998c834878384a8ebaee9bc02e0d598
riccardotrebbi <riccardo.trebbi89@gmail.com>
2018-03-08 15:08:16 +0800

    Changes

--- popup.js
+++ popup.js
@@ -2,251 +2,145 @@
 var origCareer = null;
 var career = null;
 
-function showResults(){
-	$("#info").hide();
-	$("#results").show();	
+function showResults() {
+    $("#info").hide();
+    $("#results").show();
 }
 
-function showInfo(){
-	$("#results").hide();
-	$("#info").show();
+function showInfo() {
+    $("#results").hide();
+    $("#info").show();
 }
 
-function showError(message) {
-	hideMessages();
-	$("#messages").show();
-	$("#error").show();
-	$("#error").html(message);
+function addExamToCareer(row) {
+
+    var gradeElem = $(row).find("input[name=grade]");
+    var creditsElem = $(row).find("input[name=credits]");
+
+    var gradeText = gradeElem.val();
+    var creditsText = creditsElem.val();
+
+    if (!gradeText && !creditsText) {
+        return true;
+    }
+
+    var credits = parseInt(creditsText);
+    var grade = parseInt(gradeText);
+
+    var exam = new Exam(credits);
+    exam.setGrade(grade);
+    career.addExam(exam);
+
+    return true;
 }
 
-function showWarning(message) {
-	hideMessages();
-	$("#messages").show();
-	$("#warning").show();
-	$("#warning").html(message);
+function calcolaMediaFutura() {
+
+    /* Every time calcola is clicked we reset the career because we retrieve all new exams again. */
+    resetCareer();
+
+    /* Retrieve all the exams inserted */
+    var rows = $(".exam-row");
+    var resultCalculation = true;
+    for (var i = 0; i < rows.length; ++i) {
+        if (addExamToCareer(rows[i]) == false)
+            resultCalculation = false;
+    }
+
+    updateData();
+
+    return false;
 }
 
-function hideMessages(){
-	$("#messages").hide();
-	$("#messages").children().hide();
-}
-
-function hideError() {
-	$("#error").hide();
-}
-
-function validateGrade(grade) {
-	
-	if(isNaN(grade)){
-		return "Il voto deve essere un numero!";
-	}
-	
-	if(grade < 18 || grade > 30)
-	{
-		return "Il voto deve essere compreso tra 18 e 30!";
-	}
-	
-	return null;
-}
-
-function validateCredits(credits) {
-	
-	if(isNaN(credits)){
-		return "I crediti devono essere un numero!";
-	}
-	
-	if(credits <= 0)
-	{
-		return "I crediti non possono essere nulli o negativi!";
-	}
-	
-	return null;
+function initializeDocument() {
+    $("#info").hide();
+    $("#results").hide();
 }
 
 
-function addExamToCareer(row){
-	
-	var gradeElem = $(row).find("input[name=grade]");
-	var creditsElem = $(row).find("input[name=credits]");
-	
-	var gradeText = gradeElem.val();
-	var creditsText = creditsElem.val();
-	
-	if(!gradeText && !creditsText){
-		showWarning("Le righe vuote sono state ignorate.");
-		return true;
-	}
-	
-	var credits = parseInt(creditsText);
-	var grade = parseInt(gradeText);
-	
-	var errorGrade = validateGrade(grade);
-	if(errorGrade != null){
-		$(gradeElem).parent("td").addClass("error_field");
-		$(gradeElem).attr("title", errorGrade);
-	}
-	else{
-		$(gradeElem).parent("td").removeClass("error_field");
-		$(gradeElem).removeAttr("title");
-	}
-	
-	var errorCredits = validateCredits(credits);
-	if(errorCredits != null){
-		$(creditsElem).parent("td").addClass("error_field");
-		$(creditsElem).attr("title", errorCredits);
-	}
-	else{
-		$(creditsElem).parent("td").removeClass("error_field");
-		$(creditsElem).removeAttr("title");
-	}
-	
-	if(errorGrade || errorCredits)
-		return false;
-	
-	var exam = new Exam(credits);
-	exam.setGrade(grade);
-	career.addExam(exam);
-	
-	return true;	
-}
+function resetFormMediaFutura() {
 
-function focusOnFirstError() {
-	$(".error_field input").first().focus();	
-}
+    resetCareer();
+    updateData();
 
-function onCalcolaClicked(){
-	hideMessages();
-	/* Every time calcola is clicked we reset the career because we retrieve all new exams again. */
-	resetCareer();
-	
-	/* Retrieve all the exams inserted */
-	var rows = $(".new_exam");
-	var resultCalculation = true;
-	for(var i = 0; i < rows.length; ++i){
-		if(addExamToCareer(rows[i]) == false)
-			resultCalculation = false;	
-	}
-	
-	if(!resultCalculation)
-		showError("Uno o più campi non validi!");
-	else
-		updateData();
-		
-	focusOnFirstError();
-	
-	return false;	
-}
-
-function initializeDocument(){
-	$("#info").hide();
-	$("#results").hide();
-	hideMessages();
-}
-
-
-function onResetClicked(){
-	resetCareer();
-	hideMessages()
-	updateData();
-	
-	/* Remove all rows except one */
-	$(".new_exam").each(function(index, element) {
-		if(index > 0){
-			element.remove();
-		}
+    /* Remove all rows except one */
+    $(".exam-row").each(function (index, element) {
+        if (index > 0) {
+            element.remove();
+        }
     });
-	
-	/* Empty fields in the first row */
-	resetRow($(".new_exam"));
 
-	return false;
-}
-
-function resetRow(elem) {
-	$(elem).find("input").val("");
-	$(elem).find("td").removeClass("error_field");
-	$(elem).find("input").removeAttr("title");
+    return false;
 }
 
 function resetCareer() {
-	if(origCareer == null)
-		showError("E' successo qualcosa di brutto! Prova a ricaricare la pagina.");
-	career = cloneCareer(origCareer);
+    if (origCareer) {
+        career = cloneCareer(origCareer);
+    }
 }
 
-function updateData(){
+function updateData() {
     $("#studentName").text(career.name);
-	$("#media").html(career.getMedia());
-	$("#media_centodieci").html(career.getMediaCentoDieci());
-	$("#cfu").html(career.getCFU());
+    $("#media").html(career.getMedia());
+    $("#media_centodieci").html(career.getMediaCentoDieci());
+    $("#cfu").html(career.getCFU());
 }
 
-function onDataReceived(response){
-	if(response.career == null){
-		showInfo();
-	}
-	else{
-		origCareer = cloneCareer(response.career);
-		resetCareer();
-		showResults();
-		updateData();
-	}
+function onDataReceived(response) {
+    if (response.career == null) {
+        showInfo();
+    }
+    else {
+        origCareer = cloneCareer(response.career);
+        resetCareer();
+        showResults();
+        updateData();
+    }
 }
 
-function addNewExamRow(){
-	// Columns
-	var gradeInput = $(".grade-input").first().clone().val("");
-	var creditsInput = $(".credits-input").first().clone().val("");
-	var removeButton = $(".remove").first().clone();
-	
-	// Row
-	var row = $("<tr>").addClass("new_exam").append($("<td>").append(gradeInput));
-	$(row).append($("<td>").append(creditsInput));
-	$(row).append($("<td>").append(removeButton));
+function addNewExamRow() {
 
-	$(".input_table tr:last").after(row);
-	
-	addRemoveListeners();
-	return false;
+    var row = $(".exam-row").first().clone();
+    row.find('.remove-btn').on('click', removeExamRowHandler);
+    row.find('input').val('');
+    $(".exam-form .rows").append(row);
+
+    return false;
 }
 
-
-function addRemoveListeners(){
-	$('.remove').on("click", function() {
-		if($(".new_exam").length > 1){
-			$(this).parent().parent("tr").remove();
-		}
-		else if ($(".new_exam").length == 1){
-			resetRow($(this).parent().parent("tr"));
-		}
-		return false;
-	});
+function removeExamRowHandler() {
+    if ($(".exam-row").length > 1) {
+        $(this).parents('.exam-row').remove();
+    }
 }
 
-$(document).ready(function() {
-	
-	$("#reset").click(onResetClicked);
-	
-	chrome.tabs.getSelected(null, function(tab) {
-	  chrome.tabs.sendMessage(tab.id, {execute: "calculate"}, function(response) {
-			onDataReceived(response);
-	  });
-	});
-	
-	addRemoveListeners();
-	
-	$("#add").on("click", addNewExamRow);
-	$("#add").attr("title", "Aggiungi nuovo esame");
-	$("#calcola").on("click", onCalcolaClicked);
-	
-	// Press enter is like calcola clicked
-	$(document).keypress(function(e) {
-		if(e.which == 13) {
-			if($(":focus").is("input"))
-				onCalcolaClicked();
-		}
-	});
-	
-	initializeDocument();
+$(document).ready(function () {
+
+    chrome.tabs.getSelected(null, function (tab) {
+        chrome.tabs.sendMessage(tab.id, { execute: "calculate" }, function (response) {
+            onDataReceived(response);
+        });
+    });
+
+    $('.remove-btn').on('click', removeExamRowHandler);
+
+    $("#add").on("click", addNewExamRow);
+
+    var examForm = $('.exam-form');
+    examForm.on('submit', function (event) {
+        if (examForm[0].checkValidity()) {
+            calcolaMediaFutura();
+        }
+        examForm.addClass('was-validated')
+        event.preventDefault();
+        event.stopPropagation();
+    });
+    examForm.on('reset', function (event) {
+        resetFormMediaFutura();
+        examForm.removeClass('was-validated')
+    });
+
+
+    initializeDocument();
 
 });
